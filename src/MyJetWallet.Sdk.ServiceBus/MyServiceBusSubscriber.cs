45aa34648ecef10c427af818f1d21a9f1833d467
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-05-17 15:45:54 +0300

    update

--- src/MyJetWallet.Sdk.ServiceBus/MyServiceBusSubscriber.cs
+++ src/MyJetWallet.Sdk.ServiceBus/MyServiceBusSubscriber.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 using DotNetCoreDecorators;
 using MyServiceBus.Abstractions;
 using MyServiceBus.TcpClient;
+using SimpleTrading.ServiceBus.CommonUtils.Serializers;
 
 namespace MyJetWallet.Sdk.ServiceBus
 {
     public class MyServiceBusSubscriber<T> : ISubscriber<T>, ISubscriber<IReadOnlyList<T>>
     {
-        private readonly Func<ReadOnlyMemory<byte>, T> _deserializer;
         private readonly bool _batchSubscribe;
 
         private readonly List<Func<T, ValueTask>> _list = new();
@@ -31,14 +31,14 @@ namespace MyJetWallet.Sdk.ServiceBus
 
         private async ValueTask HandlerSingle(IMyServiceBusMessage data)
         {
-            var itm = _deserializer(data.Data);
+            var itm = data.Data.ByteArrayToServiceBusContract<T>();
             foreach (var subscribers in _list)
                 await subscribers(itm);
         }
 
         private async ValueTask HandlerBatch(IConfirmationContext ctx, IReadOnlyList<IMyServiceBusMessage> data)
         {
-            var items = data.Select(e => _deserializer(e.Data)).ToList();
+            var items = data.Select(e => e.Data.ByteArrayToServiceBusContract<T>()).ToList();
 
             foreach (var callback in _listBatch)
             {
